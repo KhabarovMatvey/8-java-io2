5b8732b146f61601fe7a1c554749f8012b4a7094
KhabarovMatvey <matvey7337@gmail.com>
2025-12-02 01:38:13 +0500

    task08

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,27 @@
 package com.example.task02;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
         /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
         */
-
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static List<Path> listFiles(Path rootDir) throws IOException {
+        if (!Files.exists(rootDir) || !Files.isDirectory(rootDir)) {
+            throw new IllegalArgumentException("Указанный путь не существует или не является директорией");
+        }
 
-        return null;
+        try (Stream<Path> stream = Files.walk(rootDir)) {
+            return stream
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
     }
 }
